8970ac8e6166417a29961b574f7531d7df9d3dfc
Matheus Godoi <hi.developmatt@gmail.com>
2024-12-27 16:01:39 -0300

    refactor - implements a specific class to extract home links

---
+++ src/home-page-news-links-extractor/home-page-news-links-extractor.class.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { HtmlManipulator } from "../lib/html-manipulator.class";
+import { CONFIG } from "../config/config";
+import { HomePageNewsLinksExtractorInterface } from "./home-page-news-links-extractor.interface";
+
+export class HomePageNewsLinksExtractor implements HomePageNewsLinksExtractorInterface {
+  private htmlManipulator: HtmlManipulator;
+
+  constructor(
+    private readonly newsHomePageUrl: string,
+    private readonly headlinesLinksSelector: string
+  ) {
+    this.htmlManipulator = new HtmlManipulator();
+  }
+
+  async getPageContent(url: string) {
+    return await axios.get(url).then((res) => res.data);
+  }
+
+  async extractNewsLinksFromHomePage(homeContent: string, selector: string) {
+    this.htmlManipulator.load(homeContent.toString());
+    const feedPosts = this.htmlManipulator
+      .querySelectorAll(selector)
+      .map((_: number, item: any) => {
+        return this.htmlManipulator.getAttribute(item, "href");
+      });
+
+    const headlinesLinks: string[] = Array.from(new Set(feedPosts)).slice(
+      0,
+      CONFIG.newsLimitPerSite ?? 5
+    ) as string[];
+
+    return headlinesLinks;
+  }
+
+  async extract(): Promise<string[]> {
+    const homeContent = await this.getPageContent(this.newsHomePageUrl);
+    const headlinesLinks = await this.extractNewsLinksFromHomePage(
+      homeContent,
+      this.headlinesLinksSelector
+    );
+    return headlinesLinks;
+  }
+}
