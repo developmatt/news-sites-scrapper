1b7e284628ca980c97ad58885ccfdc2f98b57c24
Matheus Godoi <godoi.matheus@bcg.com>
2025-01-14 14:18:07 -0300

    feature - add isr to news page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { getHomePageNewsAction } from "./actions/getHomePageNews";
 import { NewsList } from "./components/news-list";
-import { CONFIG } from "./config/config";
+import { unstable_cache } from 'next/cache'
+
+const getPosts = unstable_cache(
+  async () => {
+    return await getHomePageNewsAction()
+  },
+  ['posts'],
+  { revalidate: 1, tags: ['posts'] }
+)
 
 export default async function Page() {
-  const posts = await getHomePageNewsAction()
-
-  console.log(">env", CONFIG.isProd)
-
+  const posts = await getPosts()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
